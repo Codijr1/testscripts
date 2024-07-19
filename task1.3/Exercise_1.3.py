1a17517cb3a5257b0b5f447bb498134275b66daa
Codi <cjrobards@gmail.com>
2024-07-19 19:44:26 -0400

    1.3 final?

--- task1.3/Exercise_1.3.py
+++ task1.3/Exercise_1.3.py
@@ -1,4 +1,6 @@
-# task 1.3
+#task 1.3
+
+#initialize values
 recipes_list = []
 ingredients_list = []
 
@@ -6,16 +8,15 @@ def take_recipe():
     name = input("Name of dish: ")
     cooking_time = int(input("Cooking time in minutes: "))
 
-    #entering ingredients
+#entering ingredients
     ingredients = []
-    print("Enter the recipe's ingredients one at a time, and enter 'done' when finished: ")
+    print("Enter the recipe's ingredients one at a time, and type 'done' when finished: ")
 
     while True:
         ingredient = input("Enter an ingredient: ")
         if ingredient.lower() == 'done':
             break
         ingredients.append(ingredient)
-
 
     recipe = {
         'name': name,
@@ -25,18 +26,43 @@ def take_recipe():
 
     return recipe
 
+#get the number of recipes to enter
 n = int(input("How many recipes would you like to enter? Type a whole number: "))
+
 for i in range(n):
     print(f"\nEntering details for recipe {i + 1}:")
     recipe = take_recipe()
+    
+    for ingredient in recipe['ingredients']:
+        if ingredient not in ingredients_list:
+            ingredients_list.append(ingredient)
+    
+#ppend recipe to master list
     recipes_list.append(recipe)
 
-#printing results
-
-print("\nRecipes:")
+#printing recipes with difficulty
+print("\nRecipes with Difficulty:")
 for recipe in recipes_list:
+    if recipe['cooking_time'] < 10:
+        if len(recipe['ingredients']) < 4:
+            difficulty = 'Easy'
+        else:
+            difficulty = 'Intermediate'
+    else:
+        if len(recipe['ingredients']) < 4:
+            difficulty = 'Intermediate'
+        else:
+            difficulty = 'Hard'
+    
+#printing recipe details
     print(f"\nRecipe: {recipe['name']}")
-    print(f"Cooking time: {recipe['cooking_time']} minutes")
+    print(f"Cooking Time (minutes): {recipe['cooking_time']}")
     print("Ingredients:")
     for ingredient in recipe['ingredients']:
         print(f"- {ingredient}")
+    print(f"Difficulty level: {difficulty}")
+
+#printing unique ingredients list
+print("\nIngredients Available Across All Recipes:")
+for ingredient in sorted(ingredients_list):
+    print(f"- {ingredient}")
